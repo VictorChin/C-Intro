0ca96f4968fad1395125b139d88225061bb4c3ba
Victor Chin <victor@objectmage.com>
2016-11-08 09:42:18 +0100

    Add MethodDemo

---
+++ HelloWorld/StringTest/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //StringTestAndRegSample();
+            HttpClient browser = new HttpClient();
+
+            var result = browser.GetStringAsync(@"http://www.google.com");
+            result.Wait(30000);
+            Console.WriteLine(result.Result); 
+            var matches = Regex.Matches(result.Result, 
+                @"(http|https|ftp)\://[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*");
+            //for (int i = 0; i < matches.Count; i++)
+            //{
+            //    Console.WriteLine($"{matches[i].Value}");
+            //}
+            foreach (Match item in matches)
+            {
+                Console.WriteLine(item.Value);
+            }
+            Console.ReadLine();
+
+
+
+        }
+
+        private static void StringTestAndRegSample()
+        {
+            Console.WriteLine("Enter a Postal Code:");
+            string input = Console.ReadLine();
+
+            bool isMatch = Regex.IsMatch(input, @"^[D-d][K-k]( |-)[1-9]{1}[0-9]{3}$");
+
+            if (isMatch)
+            { Console.WriteLine("Danish Postal Code"); }
+            else
+            { Console.WriteLine("Not Danish Postal Code"); }
+
+
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            string x = string.Empty;
+            for (int i = 0; i < 5; i++)
+            {
+                x += i.ToString();
+            }
+            watch.Stop();
+            Console.WriteLine($"using string {watch.ElapsedTicks}");
+            //Console.WriteLine("using string" + watch.ElapsedTicks);
+            //--------------------------------------------------------//
+
+            watch.Reset();
+            watch.Start();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 5; i++)
+            {
+                sb.Append(i.ToString());
+            }
+            watch.Stop();
+            Console.WriteLine($"using StringBuilder {watch.ElapsedTicks}");
+        }
+    }
+}
---
+++ MethodDemo/MethodDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo theFile;
+            long result = FileUtil.GetFileSize(
+                inKB: true,
+                outFile: out theFile,
+                fileName: @"C:\readme.txt");
+            Console.WriteLine(result);
+            if (theFile != null)
+            {
+                try
+                {
+                    StreamReader reader = theFile.OpenText();
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+              
+            }
+        }
+    }
+    class FileUtil
+    {
+
+        internal static long GetFileSize(string fileName, out FileInfo outFile, bool inKB = false)
+        {
+            FileInfo aFile = new FileInfo(fileName);
+            outFile = aFile;
+            long size = inKB ? aFile.Length / 1024 : aFile.Length;
+            return size;
+        }
+        internal static long GetFileSize(FileInfo fileName)
+        {
+            return fileName.Length;
+        }
+    }
+
+}
