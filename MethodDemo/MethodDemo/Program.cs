9673d77f10f610550907a781905496ba3853ba20
Victor Chin <victor@objectmage.com>
2016-11-08 12:58:25 +0100

    add Mod3

--- MethodDemo/MethodDemo/Program.cs
+++ MethodDemo/MethodDemo/Program.cs
@@ -11,22 +11,41 @@ namespace MethodDemo
     {
         static void Main(string[] args)
         {
-            FileInfo theFile;
-            long result = FileUtil.GetFileSize(
-                inKB: true,
-                outFile: out theFile,
-                fileName: @"C:\readme.txt");
-            Console.WriteLine(result);
+            FileInfo theFile=null;
+            try
+            {
+                long result = FileUtil.GetFileSize(
+                   inKB: true,
+                   outFile: out theFile,
+                   fileName: @"C:\readme.txt");
+                Console.WriteLine(result);
+            }
+            catch (ArgumentOutOfRangeException ex )
+            {
+                Console.WriteLine("bad argument:" + ex.ParamName);
+                Console.WriteLine("reason:" + ex.Message);
+            }
+
+
             if (theFile != null)
             {
                 try
                 {
-                    StreamReader reader = theFile.OpenText();
+                    StreamReader reader = File.OpenText(@"c:\readme.txt");
                     Console.WriteLine(reader.ReadToEnd());
+                }
+                catch (FileNotFoundException ex)
+                {
+                    Console.WriteLine("Invalid File,look at your files again");
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
+                }
+                finally {
+                    Console.WriteLine("Finally");
                 }
               
             }
@@ -37,8 +56,12 @@ namespace MethodDemo
 
         internal static long GetFileSize(string fileName, out FileInfo outFile, bool inKB = false)
         {
+            if (fileName.EndsWith("txt"))
+                { throw new ArgumentOutOfRangeException(nameof(fileName),"I don't like txt"); }
+
             FileInfo aFile = new FileInfo(fileName);
             outFile = aFile;
+            
             long size = inKB ? aFile.Length / 1024 : aFile.Length;
             return size;
         }
---
+++ Mod3/Mod3/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GoEat(Meal.Snack);
+            DoSomething((new DateTime(2009,8,23)).DayOfWeek);
+            
+            Point A = new Point(45, 90);
+            Point B = new Point(85, 45);
+            Point C = A;
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            A.x = 100;
+            Console.WriteLine(A);
+            Console.WriteLine(C);
+
+        }
+
+        static void DoSomething(DayOfWeek today)
+        {
+            switch (today)
+            {
+                case DayOfWeek.Sunday:
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("Weekends");
+                    break;
+                case DayOfWeek.Monday:
+                case DayOfWeek.Tuesday:
+                case DayOfWeek.Wednesday:
+                case DayOfWeek.Thursday:
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Weekdays");
+                    break;
+                default:
+                    break;
+            }
+        }
+        static void GoEat(Meal what)
+        {
+            switch (what)
+            {
+                case Meal.Breakfast:
+                    Console.WriteLine("Egg and Ham");
+                    break;
+                case Meal.Lunch:
+                    Console.WriteLine("Meat");
+                    break;
+                case Meal.Dinner:
+                    Console.WriteLine("Red Meat");
+                    break;
+                case Meal.Snack:
+                    Console.WriteLine("Meat Jerky");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    enum Meal
+    {
+        Breakfast,
+        Lunch,
+        Dinner,
+        Snack
+    }
+    struct Point {
+        internal int x;
+        internal int y;
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        
+        public override string ToString() {
+            return $"x:{x}, y:{y}";
+        }
+    }
+}
