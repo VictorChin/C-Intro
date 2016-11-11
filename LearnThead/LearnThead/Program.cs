b1bd0fe4bfcd46a70bc1a9f0b5c6711c150d0380
Victor Chin <victor@objectmage.com>
2016-11-11 15:04:54 +0100

    done

---
+++ LearnThead/LearnThead/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearnThead
+{
+    class Program
+    {
+        bool done=false;
+        readonly object locker = new object();
+        static ReaderWriterLockSlim _rw = new ReaderWriterLockSlim();
+        static List<int> _items = new List<int>();
+        static Random _rand = new Random();
+        static void Main(string[] args)
+        {
+            //Thread1();
+            //Thread2();
+            //Join();
+            new Thread(Read).Start();
+            new Thread(Read).Start();
+            new Thread(Read).Start();
+
+            new Thread(Write).Start("A");
+            new Thread(Write).Start("B");
+
+        }
+
+        private static void Write(object threadID)
+        {
+            while (true)
+            {
+                int newNumber = GetRandNum(100);
+                _rw.EnterWriteLock();
+                _items.Add(newNumber);
+                _rw.ExitWriteLock();
+                Console.WriteLine("Thread " + threadID + " added " + newNumber);
+                Thread.Sleep(100);
+            }
+        }
+        static int GetRandNum(int max) { lock (_rand) return _rand.Next(max); }
+        private static void Read()
+        {
+            while (true)
+            {
+                _rw.EnterReadLock();
+                foreach (int i in _items) Thread.Sleep(10);
+                _rw.ExitReadLock();
+            }
+        }
+
+        private static void Join()
+        {
+            Thread t = new Thread(Go);
+            t.Name = "GoThread";
+            t.Start();
+            t.Join();
+            Console.WriteLine("Thread t has ended!");
+        }
+
+        static void Go()
+        {
+            for (int i = 0; i < 1000; i++) Console.WriteLine(i);
+        }
+
+        //private static void Thread2()
+        //{
+        //    Program tt = new Program();   // Create a common instance
+        //    new Thread(tt.Go).Start();
+        //    tt.Go();
+        //    //thread can 1-execute 2-be blocked  3-sleep(spinwait)
+        //}
+
+        //void Go()
+        //{
+        //    lock (locker)
+        //    {
+        //        if (!done)
+        //        {   done = true;
+        //            Console.WriteLine("Done");
+        //        }
+        //    }
+        //}
+
+        private static void Thread1()
+        {
+            Thread t = new Thread(
+                            () =>
+                            {
+                                for (int i = 0; i < 1000; i++) Console.Write("y");
+                            });          // Kick off a new thread
+            t.Start();                               // running WriteY()
+
+            // Simultaneously, do something on the main thread.
+            for (int i = 0; i < 1000; i++) Console.Write("x");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SemaphoreDemo/SemaphoreDemo/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreDemo
+{
+
+    class Program
+    {
+       
+        static SemaphoreSlim garage = new SemaphoreSlim(2);
+        static void Main(string[] args)
+        {
+            //Semaphore();
+            //PLINQ();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var t = DemoMethodAsync("http://www.yahoo.com");
+            var t2 = DemoMethodAsync("http://www.cnn.com");
+            var t3 = DemoMethodAsync("http://www.reddit.com");
+            t.Wait();
+            t2.Wait();
+            t3.Wait();
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            sw.Reset();
+            sw.Start();
+            var t4 = DemoMethodAsync("http://www.yahoo.com")
+                .ContinueWith((r) => DemoMethodAsync("http://www.cnn.com"))
+                .ContinueWith((r) => DemoMethodAsync("http://www.reddit.com"));
+            t4.Wait();
+            sw.Stop();
+            Console.WriteLine(sw.ElapsedMilliseconds);
+            Type thing = sw.GetType();
+        
+
+            Console.ReadLine();
+            
+        }
+
+        public static async Task<string> DemoMethodAsync(string url) {
+            HttpClient client = new HttpClient();
+            string s = await client.GetStringAsync(url);
+            return s;
+        }
+        private static void PLINQ()
+        {
+            IEnumerable<int> numbers = Enumerable.Range(3, 100000000 - 3);
+
+            var parallelQuery =
+              from n in numbers.AsParallel()
+              where Enumerable.Range(2, (int)Math.Sqrt(n)).All(i => n % i > 0)
+              select n;
+
+            int[] primes = parallelQuery.ToArray();
+            Console.WriteLine(primes.Count());
+        }
+
+        private static void Semaphore()
+        {
+            Thread t = new Thread(Park);
+            Thread t1 = new Thread(Park);
+            Thread t2 = new Thread(Park);
+            Thread t3 = new Thread(Park);
+            Thread t4 = new Thread(Park);
+            Console.WriteLine(garage.CurrentCount);
+            t.Start();
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            t4.Start();
+            Console.WriteLine("done");
+        }
+
+        static void Park() {
+            garage.Wait();
+            Console.WriteLine("Parked");
+            Thread.Sleep(5000);
+            Console.WriteLine("Leaving");
+            garage.Release();
+        }
+    }
+
+  
+}
